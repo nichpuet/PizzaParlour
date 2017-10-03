8f3e1bbabdd687c9c48589f5e6a750396b866732
nichpuet <napuetz@gmail.com>
2017-10-03 13:19:40 -0400

    Add project files.

---
+++ PizzaParlour/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PizzaParlour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calcButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(119, 131);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(137, 37);
+            this.calcButton.TabIndex = 0;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(183, 50);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(72, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number Of Toppings :\r\n";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(114, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(164, 34);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Pizza Pie Palooza";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.calcButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
