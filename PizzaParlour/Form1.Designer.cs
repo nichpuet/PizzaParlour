a94a8b9daef8572513f726a3d29a8d714a348a88
nichpuet <napuetz@gmail.com>
2017-10-03 13:34:46 -0400

    uh

--- PizzaParlour/Form1.Designer.cs
+++ PizzaParlour/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.inputBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.titleLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // calcButton
@@ -68,12 +69,23 @@
             this.titleLabel.TabIndex = 3;
             this.titleLabel.Text = "Pizza Pie Palooza";
             // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(70, 199);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(131, 26);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "Your Total is: ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Coral;
             this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.inputBox);
@@ -91,6 +103,7 @@
         private System.Windows.Forms.TextBox inputBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label titleLabel;
+        public System.Windows.Forms.Label outputLabel;
     }
 }
 
--- PizzaParlour/Form1.cs
+++ PizzaParlour/Form1.cs
@@ -12,6 +12,10 @@ namespace PizzaParlour
 {
     public partial class Form1 : Form
     {
+        //Global variables for price
+        const double PIE_PRICE = 7.00;
+        const double TOPPING_PRICE = 1.25;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +23,15 @@ namespace PizzaParlour
 
         private void calcButton_Click(object sender, EventArgs e)
         {
+            // takes the input from the textbox and puts it into a variable
+            int toppingNumber;
+            toppingNumber = Convert.ToInt16(inputBox.Text);
+
+            // calculates the price
+            Double totalPrice = TOPPING_PRICE * toppingNumber + PIE_PRICE;
+            
+            //displays price
+            outputLabel.Text = "Your Total Is: "+ totalPrice.ToString("C");
 
         }
     }
